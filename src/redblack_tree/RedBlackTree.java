603da77a0b8e8374bd67dacb141dd7984451061a
Victor Andrade <victor64gaming@gmail.com>
2025-06-10 21:57:35 -0300

    RedBlackTree class structure

---
+++ src/redblack_tree/RedBlackTree.java
@@ -0,0 +1,39 @@
+package redblack_tree;
+
+public class RedBlackTree {
+
+    // ===========================
+    // VARIABLES
+    // ===========================
+
+    private Node root;
+
+    // ===========================
+    // CONSTRUCTORS
+    // ===========================
+
+    public RedBlackTree() {
+    }
+
+    public RedBlackTree(Node root) {
+        this.root = root;
+    }
+
+    // ===========================
+    // METHODS
+    // ===========================
+
+    public void insert(int data) {
+    }
+
+    public void delete(int data) {
+    }
+
+    // ===========================
+    // GETTERS
+    // ===========================
+
+    public Node getRoot() {
+        return root;
+    }
+}
